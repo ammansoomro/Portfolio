1a39abef57e77f84ef3ef23c3e57f1c244c86d83
ammansoomro <amman.soomro.as@gmail.com>
2023-08-06 03:35:35 +0500

    Added Required Schemas

--- amman-portfolio/schemas/index.js
+++ amman-portfolio/schemas/index.js
@@ -1,1 +1,18 @@
-export const schemaTypes = []
+import testimonials from "./testimonials"
+import abouts from "./abouts"
+import brands from "./brands"
+import contact from "./contact"
+import experiences from "./experiences"
+import skills from "./skills"
+import works from "./works"
+import workExperience from "./workExperience"
+export const schemaTypes = [
+    abouts,
+    brands,
+    contact,
+    experiences,
+    skills,
+    works,
+    workExperience,
+    testimonials
+]
