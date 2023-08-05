965444c648acd7995fa01b3b418542d8ecb88a01
ammansoomro <amman.soomro.as@gmail.com>
2023-08-06 04:53:48 +0500

    Made Changes

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: 'w7w7umz3',
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: 'skjq6Cjolq5BHxOpvfA4xfMe2nc47aysr0WysjEuxM7kec0BgCUPVCVNaf3Z3PJBOYSdVuiQIuOLyVWER96lGdiH05JrBGyvHq9PsXWMB6DFrjzbdO3YFQUhcWSLgKR4nLF2lDJia4As7pQgBaAunLShxKSrFdJmTZRAKz94l0D4XuM4ouoH',
 });
 
 const builder = imageUrlBuilder(client);
