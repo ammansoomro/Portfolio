13bdc9bb4a3df2ab9c48a6bf8903f67d4b940440
ammansoomro <amman.soomro.as@gmail.com>
2023-08-06 16:27:30 +0500

    Updated Skills Schema

--- amman-portfolio/schemas/skills.js
+++ amman-portfolio/schemas/skills.js
@@ -1,26 +1,33 @@
-export default{
-    name:'skills',
-    title:'Skills',
+export default {
+    name: 'skills',
+    title: 'Skills',
     type: 'document',
-    fields:[
+    fields: [
         {
-            name:'name',
-            title:'Name',
-            type:'string'
+            name: 'name',
+            title: 'Name',
+            type: 'string'
         },
         {
-            name:'bgColor',
-            title:'BgColor',
-            type:'string'
+            name: 'bgColor',
+            title: 'BgColor',
+            type: 'string',
+            options: {
+                list: [
+                    { title: 'Light Gray', value: '#EDF2F7' },
+                    // Add more color options if needed
+                ],
+            },
+            initialValue: '#EDF2F7' // Set the initial/default value to #EDF2F7
         },
         {
-            name:'icon',
-            title:'Icon',
+            name: 'icon',
+            title: 'Icon',
             type: 'image',
             options: {
-              hotspot: true,
+                hotspot: true,
             },
         },
-        
+
     ]
 }
