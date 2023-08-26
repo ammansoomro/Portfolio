81a10c410104ff558331aa97ab600ca3e48fe12e
ammansoomro <amman.soomro.as@gmail.com>
2023-08-26 12:32:00 +0500

    Updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,9 @@ const App = () => (
   <div className="app">
     <Navbar />
     <Header />
-    <About />
-    <Work />
+    {/* <About /> */}
     <Skills />
+    <Work />
     <Testimonial />
     <Footer />
   </div>
