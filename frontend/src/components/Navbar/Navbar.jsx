a02a60b07e4dc3e2dd6accd7619f6dc631f758dd
muhammad-amman <63865428+ammansoomro@users.noreply.github.com>
2025-02-01 00:40:28 +0500

    Updated Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,46 +1,12 @@
 import React, { useState } from "react";
-import { HiMenuAlt4, HiX } from "react-icons/hi";
-import { motion } from "framer-motion";
 
 import "./Navbar.scss";
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
-
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <span>A</span>mman
-      </div>
-      <ul className="app__navbar-links">
-        {["home", "skills", "work", "contact"].map((item) => (
-          <li className="app__flex p-text" key={`link-${item}`}>
-            <div />
-            <a href={`#${item}`}>{item}</a>
-          </li>
-        ))}
-      </ul>
-
-      <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
-
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: "easeOut" }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul>
-              {["home", "about", "work", "skills", "contact"].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {item}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </motion.div>
-        )}
+        <span>A</span>mman <span>S</span>Soomro
       </div>
     </nav>
   );
