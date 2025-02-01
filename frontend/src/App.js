ffe8f47c59b42da8646a06805148565ec519ebf6
muhammad-amman <63865428+ammansoomro@users.noreply.github.com>
2025-02-01 09:54:53 +0500

    Removed Testimonals and Added About Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-import { Footer, Header, Skills, Testimonial, Work } from "./container";
+import { About, Footer, Header, Skills, Work } from "./container";
 import "./App.scss";
 
 const App = () => (
   <div className="app">
     <Header />
+    <About />
     <Skills />
     <Work />
-    <Testimonial />
     <Footer />
   </div>
 );
