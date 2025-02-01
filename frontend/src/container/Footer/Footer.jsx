b776d3f6a75d84bb33cbf048fde2bda0d9a3ed3b
muhammad-amman <63865428+ammansoomro@users.noreply.github.com>
2025-02-01 11:42:34 +0500

    Used Design System Classes in Footer

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -6,47 +6,43 @@ import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <>
+    <div className="flex flex-center flex-col gap-xl">
       <h2 className="head-text color-primary-text">
         <span className="color-primary">Connect</span> with me
       </h2>
 
-      <div class="app__footer-cards">
+      <div class="flex flex-col flex-align-center w-4-5 gap-base">
         <a
           href="https://firebasestorage.googleapis.com/v0/b/fastcentralhub.appspot.com/o/Amman_Soomro_Resume.pdf?alt=media&token=0eb046e5-b81d-4f48-a743-7de1918e51d0"
-          class="p-text"
           download=""
         >
-          <div class="app__footer-card bg-secondary-300">
+          <div class="app__footer-card border-radius-m flex flex-justify-start p-base flex-align-center gap-m bg-secondary-300">
             <img src={images.Resume} alt="mobile" />
-            <span className="neutral-1000 content-accent">Download Resume</span>
+            <span className="base-black content-accent">Download Resume</span>
           </div>
         </a>
-        <a href="mailto:amman.soomro.as@gmail.com" class="p-text">
-          <div class="app__footer-card bg-warning-300">
+        <a href="mailto:amman.soomro.as@gmail.com">
+          <div class="app__footer-card border-radius-m flex flex-justify-start p-base flex-align-center gap-m bg-warning-300">
             <img src={images.Gmail} alt="email" />
-            <span className="neutral-1000 content-accent">Email Me</span>
+            <span className="base-black content-accent">Email Me</span>
           </div>
         </a>
-        <a
-          href="https://api.whatsapp.com/send?phone=%2B923138501180&amp;text&amp;app_absent=0"
-          class="p-text"
-        >
-          <div class="app__footer-card bg-success-300">
+        <a href="https://api.whatsapp.com/send?phone=%2B923138501180&amp;text&amp;app_absent=0">
+          <div class="app__footer-card border-radius-m flex flex-justify-start p-base flex-align-center gap-m bg-success-300">
             <img src={images.Whatsapp} alt="mobile" />
-            <span className="neutral-1000 content-accent">Text Me</span>
+            <span className="base-black content-accent">Text Me</span>
           </div>
         </a>
       </div>
       <div className="content color-primary-text">
         Copyright © {new Date().getFullYear()} | Crafted with 💛 by Amman Soomro
       </div>
-    </>
+    </div>
   );
 };
 
 export default AppWrap(
-  MotionWrap(Footer, "app__footer"),
+  MotionWrap(Footer, "flex full-width flex-col"),
   "contact",
   "bg-color-primary-bg"
 );
