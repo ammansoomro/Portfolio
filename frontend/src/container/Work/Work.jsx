ca60499a6402f88933a7a697df214f01cd86081d
muhammad-amman <63865428+ammansoomro@users.noreply.github.com>
2025-02-02 18:37:23 +0500

    Updated Work Section to Show Github and Website Link

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -73,49 +73,31 @@ const Work = () => {
           >
             <div className="app__work-img flex flex-center">
               <img src={urlFor(work.imgUrl)} alt={work.name} />
-
-              <motion.div
-                whileHover={{ opacity: [0, 1] }}
-                transition={{
-                  duration: 0.25,
-                  ease: "easeInOut",
-                  staggerChildren: 0.5,
-                }}
-                className="app__work-hover flex flex-center"
-              >
-                {work.projectLink && (
-                  <a href={work.projectLink} target="_blank" rel="noreferrer">
-                    <motion.div
-                      whileInView={{ scale: [0, 1] }}
-                      whileHover={{ scale: [1, 0.9] }}
-                      transition={{ duration: 0.25 }}
-                      className="flex flex-center"
-                    >
-                      <AiFillEye />
-                    </motion.div>
-                  </a>
-                )}
-
-                {work.codeLink && (
-                  <a href={work.codeLink} target="_blank" rel="noreferrer">
-                    <motion.div
-                      whileInView={{ scale: [0, 1] }}
-                      whileHover={{ scale: [1, 0.9] }}
-                      transition={{ duration: 0.25 }}
-                      className="flex flex-center"
-                    >
-                      <AiFillGithub />
-                    </motion.div>
-                  </a>
-                )}
-              </motion.div>
             </div>
 
             <div className="app__work-content full-width flex flex-col flex-center p-sm">
               <div className="flex flex-col gap-s">
-                <h4 className="feature-bold color-primary-text text-center mt-base">
-                  {work.title}
-                </h4>
+                <div className="flex flex-justify-between flex-align-center  mt-sm">
+                  <h4 className="feature-bold color-primary-text text-center">
+                    {work.title}
+                  </h4>
+                  <div className="flex gap-m">
+                    {work.projectLink && (
+                      <a
+                        href={work.projectLink}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <AiFillEye />
+                      </a>
+                    )}
+                    {work.codeLink && (
+                      <a href={work.codeLink} target="_blank" rel="noreferrer">
+                        <AiFillGithub />
+                      </a>
+                    )}
+                  </div>
+                </div>
                 <p className="content color-primary-text ">
                   {work.description}
                 </p>
